c126261ed911e01c7636ee0acd3ef460a17ffbc0
harshitpratapsingh01 <harshit.singh@appinventiv.com>
2023-07-14 08:25:01 +0530

    db

---
+++ src/models/schema.ts
@@ -0,0 +1,72 @@
+import Sequelize from "sequelize";
+import { sqlize, dbconnection } from "../core/connection";
+
+const User = sqlize.define('users', {
+    username: {
+        type: Sequelize.STRING
+    },
+    createdAt: {
+        type: Sequelize.DATE,
+        default: Date.now()
+    },
+    updatedAt: {
+        type: Sequelize.DATE,
+        default: Date.now()
+    }
+});
+
+const Photos = sqlize.define('photos', {
+    url: {
+        type: Sequelize.STRING
+    },
+    user_id: {
+        type: Sequelize.INTEGER
+    },
+    createdAt: {
+        type: Sequelize.DATE,
+        default: Date.now()
+    },
+    updatedAt: {
+        type: Sequelize.DATE,
+        default: Date.now()
+    }
+});
+
+const Comments = sqlize.define('comments', {
+    contents: {
+        type: Sequelize.STRING
+    },
+    user_id: {
+        type: Sequelize.INTEGER
+    },
+    photo_id: {
+        type: Sequelize.INTEGER
+    },
+    createdAt: {
+        type: Sequelize.DATE,
+        default: Date.now()
+    },
+    updatedAt: {
+        type: Sequelize.DATE,
+        default: Date.now()
+    }
+});
+
+const Likes = sqlize.define('likes', {
+    comment_id: {
+        type: Sequelize.INTEGER
+    },
+    totallikes: {
+        type: Sequelize.INTEGER
+    },
+    createdAt: {
+        type: Sequelize.DATE,
+        default: Date.now()
+    },
+    updatedAt: {
+        type: Sequelize.DATE,
+        default: Date.now()
+    }
+});
+
+export { User, Photos, Comments, Likes };
